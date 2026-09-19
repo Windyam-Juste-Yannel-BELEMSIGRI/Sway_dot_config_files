a60331a6ef4b4f7146411632f2ede6024476bc49
Windyam-Juste-Yannel-BELEMSIGRI <yannelbelemsigri9@gmail.com>
2026-09-19 09:31:12 +0000

    initial_configaration

---
+++ sway/lock.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+swaylock \
+    --color 1c1c1e \
+    --inside-color 2a2a2e \
+    --ring-color 7dd3c0 \
+    --line-color 1c1c1e \
+    --text-color e8e8ec \
+    --key-hl-color 7dd3c0 \
+    --separator-color 00000000 \
+    --font "JetBrains Mono" \
+    --indicator-radius 90 \
+    --indicator-thickness 8 \
+    --clock \
+    --timestr "%H:%M" \
+    --datestr "%A %d %B"
